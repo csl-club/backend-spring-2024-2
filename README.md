ef14a8fb7d5389ddf3a489fbacc567423cf40640
Salva <atlas200x@gmail.com>
2024-09-22 17:31:23 -0500

    Second commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# backend-spring-2024-2
+Template for projects with Spring Boot as Web Backend
---
+++ src/main/java/com/github/csl/backendspring20242/BackendSpring20242Application.java
@@ -0,0 +1,13 @@
+package com.github.csl.backendspring20242;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpring20242Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpring20242Application.class, args);
+    }
+
+}
